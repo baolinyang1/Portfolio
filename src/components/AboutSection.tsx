4d347c21e2c08e4b192d0be4fb71edaf7d2666af
baolin yang <baoliny961@gmail.com>
2026-03-22 22:52:29 -0600

    update

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -10,7 +10,7 @@ export const AboutSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
             <h3 className="text-2xl font-semibold">
-              Passionate Web Developer & Tech Creator
+              Passionate Web Developer & AI Developer & Tech Creator
             </h3>
             <p className="text-muted-foreground">Love to build things and learn new things</p>
             <p>Utilize my best skills to build things that are useful and impact people's lives</p>
